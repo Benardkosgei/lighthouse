51cfe5265719ef14d3a476b2efa5a822b73fee50
Benardkosgei <benardkosgei96@gmail.com>
2022-01-25 14:06:08 +0300

    First Commit

---
+++ manage/include/footer.php
@@ -0,0 +1,35 @@
+  <div class="section-body">
+            <footer class="footer">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-6 col-sm-12">
+                            <a href="templateshub.net">Templates Hub</a>
+                        </div>
+                        <div class="col-md-6 col-sm-12 text-md-right">
+                            <ul class="list-inline mb-0">
+                                <li class="list-inline-item"><a href="doc/index.html">Documentation</a></li>
+                                <li class="list-inline-item"><a href="javascript:void(0)">FAQ</a></li>
+                                <li class="list-inline-item"><a href="javascript:void(0)" class="btn btn-outline-primary btn-sm">Buy Now</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>    
+</div>
+
+
+<script src="assets/bundles/lib.vendor.bundle.js"></script>
+
+<script src="assets/bundles/apexcharts.bundle.js"></script>
+<script src="assets/bundles/counterup.bundle.js"></script>
+<script src="assets/bundles/knobjs.bundle.js"></script>
+<script src="assets/bundles/c3.bundle.js"></script>
+
+<script src="assets/js/core.js"></script>
+<script src="assets/js/page/project-index.js"></script>
+</body>
+
+<!-- soccer/project/  07 Jan 2020 03:37:22 GMT -->
+</html>
---
+++ manage/include/left_side.php
@@ -0,0 +1,40 @@
+    <div id="left-sidebar" class="sidebar ">
+        <h5 class="brand-name">Light House <a href="javascript:void(0)" class="menu_option float-right">
+            <i class="icon-grid font-16" data-toggle="tooltip" data-placement="left" title="Grid & List Toggle"></i></a></h5>
+        <nav id="left-sidebar-nav" class="sidebar-nav">
+            <ul class="metismenu">
+                <li class="g_heading">Project</li>
+                <li class="dashboard"><a href="index.php"><i class="fa fa-dashboard"></i><span>Dashboard</span></a></li>                        
+                <li class="project-list"><a href="project-list.php"><i class="fa fa-list-ol"></i><span>Project list</span></a></li>
+                <li class="project-taskboard"><a href="project-taskboard.php"><i class="fa fa-calendar-check-o"></i><span>Taskboard</span></a></li>
+                <li class="project-ticket"><a href="project-ticket.php"><i class="fa fa-list-ul"></i><span>Ticket List</span></a></li>
+                <li class="project-ticket-details"><a href="project-ticket-details.php"><i class="icon-tag"></i><span>Ticket Details</span></a></li>
+                <li class="project-clients"><a href="project-clients.php">
+                    <i class="fa fa-user"></i><span>Clients</span></a>
+                   </li>
+                <li class="project-todo"><a href="project-todo.php">
+                    <i class="fa fa-check-square-o"></i><span>Todo List</span></a>
+                    </li>
+                <li class="g_heading">App</li>
+                <li class="app-calendar"><a href="app-calendar.php"><i class="fa fa-calendar"></i><span>Calendar</span></a></li>
+                <li class="app-chat"><a href="app-chat.php"><i class="fa fa-comments"></i><span>Chat</span></a></li>
+                <li class="app-contact"><a href="app-contact.php"><i class="fa fa-address-book"></i><span>Contact</span></a></li>
+                <li class="app-filemanager"><a href="app-filemanager.php"><i class="fa fa-folder"></i><span>FileManager</span></a></li>
+                <li class="app-setting"><a href="app-setting.php"><i class="fa fa-gear"></i><span>Setting</span></a></li>
+                <li class="page-gallery"><a href="page-gallery.php"><i class="fa fa-photo"></i><span>Gallery</span></a></li>
+                <li class="authentication">
+                    <a href="javascript:void(0)" class="has-arrow arrow-c"><i class="fa fa-lock"></i><span>Authentication</span></a>
+                    <ul>
+                        <li><a href="login.php">Login</a></li>
+                        <li><a href="register.php">Register</a></li>
+                        <li><a href="forgot-password.php">Forgot password</a></li>
+                        <li><a href="404.php">404 error</a></li>
+                        <li><a href="500.php">500 error</a></li>   
+                    </ul>
+                </li>
+                <li class="g_heading">Support</li>
+                <li><a href="javascript:void(0)"><i class="fa fa-support"></i><span>Need Help?</span></a></li>
+                <li><a href="javascript:void(0)"><i class="fa fa-tag"></i><span>ContactUs</span></a></li>
+            </ul>
+        </nav>        
+    </div>
